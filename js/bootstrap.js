0beb34b6a52efc6e83944235bd7c673ed421edec
Robert Clark <rbclark@mitre.org>
2014-06-04 16:43:33 -0400

    Added check for logged in cookie before users can see pages requiring login.

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1430,15 +1430,50 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
  * ======================================================================== */
 
 function login(form) {
-    if (form.username.value == "somesupersecretlonguser") {
-        if (form.password.value == "nobodycanhackmebecausei'mawesome!") {
-            location = "loggedin.html"
-        } else {
-            alert("Invalid Password")
-        }
+  if (form.username.value == "somesupersecretlonguser") {
+    if (form.password.value == "nobodycanhackmebecausei'mawesome!") {
+      createCookie('extremelysecurecookie','loggedin',7)
+      location = "loggedin.html"
     } else {
-        alert("Invalid Username")
+      eraseCookie('extremelysecurecookie')
+      alert("Invalid Password")
     }
+  } else {
+    eraseCookie('extremelysecurecookie')
+    alert("Invalid Username")
+  }
+}
+
+function loggedin() {
+  var x = readCookie('extremelysecurecookie')
+  if (x != 'loggedin') {
+    location = "index.html"
+  }
+}
+
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else var expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  createCookie(name,"",-1);
 }
 
 +function ($) {
